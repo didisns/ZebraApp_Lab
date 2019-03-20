23a6fd35243d3af84e2cd0e7f0c1ef37b722d70d
didisns <43500979+didisns@users.noreply.github.com>
2019-03-20 09:39:47 +0100

    Initial commit

---
+++ computeMeanOnTrials.m
@@ -0,0 +1,306 @@
+function computeMeanOnTrials (app)
+% This function is evoched in the presence of multiple trials. 
+% Data are averaged on the trial number
+% Finally, it computes metrics of the evoched response
+% February 5, 2017
+% Debug on January, 3 2018.
+% Made some check to improve reliability for operation in the MeyerVEP mode
+% Genuary 29, 2019. Adapted to app.
+disp('Compute mean on trials');
+computeMeanData (app)
+
+skipFlag = get (app.skip1trialCk,'value');
+if skipFlag, trialBegins = 2;
+else
+    trialBegins = 1;
+end
+
+% load from the GUI all the necessary parameters
+
+syCh = app.synchCh.Value;
+exCh = app.excludeCh.Value;
+flagExCh = app.excludeChFlag.Value;
+flagSynch = app.internalSynchCk.Value;
+flagTemplate = app.templateCk.Value;
+flagDouble = app.doubleStimCk.Value;
+
+sweepDuration = app.sp * (app.dtaLen-1);    % duration of each trial
+
+% fix the following for meyer veps
+%             handles.ntemp = 1;
+%             if flagDouble, handles.ntemp = 2;
+%             end
+
+halfSweep = int32 (app.dtaLen/2);       % this can be easily generalize to multiple stim per sweep
+halfSweepSPG = int32(app.spgl/2);
+
+% window for template computation and fit
+templFromI = int32(app.templateFrom.Value/app.sp)+1 - app.timeOffset_i; %GAB 2019/02/24: offset added
+templToI = int32(app.templateTo.Value/app.sp)+1 - app.timeOffset_i; %GAB 2019/02/24: offset added
+PWfromI = int32(app.winPWleft.Value/app.sp)+1 - app.timeOffset_i; %GAB 2019/02/24: offset added  % window for power computation
+PWtoI = int32(app.winPWright.Value/app.sp)+1 - app.timeOffset_i; %GAB 2019/02/24: offset added
+% now check that all thee pointers have legal values, i.e. are compatible
+% with the sweep length
+if (templToI > app.dtaLen), templToI = app.dtaLen;
+end
+if (PWtoI > app.dtaLen), PWtoI = app.dtaLen;
+end
+
+templateLen = templToI - templFromI + 1;
+tempTempl = zeros(1,templateLen);    
+templateConvolutionLen = PWtoI - PWfromI +1;
+tempTemplConv = zeros(1,templateConvolutionLen);
+app.templateConv = [];
+
+% windows for baseline computation and response
+blFrom = int32(app.baselineFrom.Value/app.sp)+1 - app.timeOffset_i; %GAB 2019/02/24: offset added
+blTo = int32(app.baselineTo.Value/app.sp)+1 - app.timeOffset_i; %GAB 2019/02/24: offset added
+respFrom = int32(app.respFrom.Value/app.sp)+1 - app.timeOffset_i; %GAB 2019/02/24: offset added
+respTo = int32(app.respTo.Value/app.sp)+1 - app.timeOffset_i; %GAB 2019/02/24: offset added
+
+% processing of spectrograms
+% compute the deltaT of the spectrogram
+dlt = (app.spgt(end)-app.spgt(1))/(size(app.spgt,2));
+blFromSPG = int32((app.baselineFrom.Value-app.spgt(1))/dlt) + 1;
+if (blFromSPG<1), blFromSPG=1;
+end
+blToSPG = int32((app.baselineTo.Value-app.spgt(1))/dlt) + 1;
+if (blToSPG<1), blToSPG=1;
+end
+rsFromSPG= int32((app.respFrom.Value-app.spgt(1))/dlt) + 1;
+if (rsFromSPG<1), rsFromSPG=1;
+end
+rsToSPG = int32((app.respTo.Value-app.spgt(1))/dlt) + 1;
+bltemp = [];
+rstemp = [];
+
+if get(app.autoclear,'Value')
+    app.respSummary = {};
+    app.EPcnt = 0;
+end
+
+%----enrico+gab-2018/10/09
+%set sgolay filter window
+wSgolay = 2*round(0.5*(0.0101/app.sp-1))+1;
+%----
+for i=1:app.nCh
+    % Computation of the metrics of the responses
+    % Here we exclude the trigger channel and, if it is so marked, the
+    % excluded channel.
+    
+    if (i ~= syCh || ~flagSynch) % if flagSynch is false always do the following
+        if ~(flagExCh && i == exCh) 
+            % compute evoked response from the mean trials and SPGs
+            % first: computation in the time domain
+            bltemp = [];
+            rstemp = [];
+            % compute the template all the way to the end of the
+            % convolution window for the computation of the normalized
+            % spectral power.
+            app.ntemp = 1;
+            if flagDouble, app.ntemp = 2;
+            end
+            for ii=1:app.ntemp
+                tFrom(ii) = templFromI + halfSweep * (ii-1);
+                tTo(ii) = templToI + halfSweep * (ii-1);
+                tempTempl = tempTempl + app.meanLFP (i,tFrom(ii):tTo(ii)); 
+                blFromNow(ii) = blFrom + halfSweep * (ii-1);
+                blToNow(ii) = blTo + halfSweep * (ii-1);
+                respFromNow(ii) = respFrom + halfSweep * (ii-1);
+                respToNow(ii) = respTo + halfSweep * (ii-1);
+
+                tFromPW(ii) = PWfromI + halfSweep * (ii-1);
+                tToPW(ii) = PWtoI + halfSweep * (ii-1);
+                tempTemplConv = tempTemplConv + app.meanLFP (i,tFromPW(ii):tToPW(ii));
+            end
+            
+            % now average the mean sweeps
+            template = tempTempl / app.ntemp;
+            templateConv = tempTemplConv / app.ntemp;
+            % smooth the template by Savitzky-Golay filtering
+            template = sgolayfilt(template,3,wSgolay); %previously framelength was 101
+            templateConv = sgolayfilt(templateConv,3,wSgolay);
+            
+            % align the template to baseline=0
+            blDelta = blTo - templFromI;    % number of point in baseline
+            offset = mean(template(1:blDelta));     % OK
+            template = template - offset;
+            templateConv = templateConv - offset;
+            % compute the amplitude of the template.
+            [templatePeak peak_i] = max(abs(template));      % suggestion: what about normalizing the template to 1?
+            app.templatePeakSigned(i) = template(peak_i);
+            templateConv = templateConv / templatePeak;
+            if templatePeak ~= max(template), templatePeak = -templatePeak;
+            end
+            template = template  / templatePeak;
+            %handles.templ = template;
+            app.templ(i,:) = template; %gab_2018/09/19 -> handles.templ is now a matrix with a "channel" dimension.
+            app.templateConv(i,:) = templateConv; %gab+enr_2018/10/09
+            
+            % now the template must be fitted to the corresponding segments
+            % of each trial. Each fit returns the offset and the linear
+            % scaling factor.
+            EPamplitude = [];
+            for k=trialBegins:app.nTrials
+                for ii=1:app.ntemp
+                    app.EPcnt = app.EPcnt + 1;
+                    % first extract the LFP segment to fit with the template
+                    segment(1:templateLen) = app.workLFP(i,k,tFrom(ii):tTo(ii));
+                    % Offset the segment to align the baseline to 0
+                    offset = mean(segment(1:blDelta));
+                    segment = segment - offset;
+                    %handles.segment(i,k,ii,:) = segment; %GAB&ENR 2018/10/03
+                    %X = [ones(length(segment),1) segment];
+                    %fitNow = template'\segment      
+                    x = fminsearch(@gixres,1);
+                    localEPamp = x ; % after the normalisation the following disappears! * templatePeak;
+                    
+                    app.EPamplitude(i,k,ii) = x; % this is used to plot the fitted template
+                    app.EPoffset(i,k,ii) = offset;
+                    % computation in the frequency domain by using the
+                    % power selected (index: BP4power+1) GAB 06/04/2018
+                    bltemp = [];
+                    rstemp = [];
+                    
+                    bltemp = app.bandPassed_LFP(app.BP4power+1,i,k,blFromNow(ii):blToNow(ii));
+                    %GAB 2019/02/24: add logarithm
+                    bl = 10*log10(squeeze(rms (bltemp)));  % do we really need to squeeze?
+                 
+                    %bltemp = handles.EIrat (i,k,blFromSPG + halfSweepSPG*(ii-1):blToSPG + halfSweepSPG*(ii-1));
+                    %bl = squeeze(mean (bltemp,3));
+                    rstemp = app.bandPassed_LFP(app.BP4power+1,i,k,respFromNow(ii):respToNow(ii));
+                    %GAB 2019/02/24: add logarithm
+                    rs = 10*log10(squeeze(rms (rstemp)));
+                    
+                    %rstemp = handles.EIrat (i,k,rsFromSPG + halfSweepSPG*(ii-1):rsToSPG + halfSweepSPG*(ii-1));
+                    %rs = squeeze(mean (rstemp,3));
+                    FDresp = rs-bl;
+                    % extract the band passed data
+                    tempPW = app.bandPassed_LFP(app.BP4power+1,i,k,tFromPW(ii):tToPW(ii));
+                    % multiply it by the fitted template. NO! we should
+                    % pass it through the template but not the fitted
+                    % template, otherwise the correlation between power and
+                    % amplitude is artificially injected in the metric.
+                    
+                    newTempPW =  templateConv .* squeeze(tempPW)';
+                    % and now compute the spectral power
+                    templateSPW = rms(newTempPW);
+                    
+                    % stuff everything in the output table!
+                    app.respSummary(app.EPcnt,1) = cellstr(app.file_in);  % 1: file name
+                    app.respSummary(app.EPcnt,2) = num2cell(i);               % 2: channel number
+                    app.respSummary(app.EPcnt,3) = num2cell(k);               % 3: trial
+                    app.respSummary(app.EPcnt,4) = num2cell(ii);              % 4: repeat
+
+                    app.respSummary(app.EPcnt,5) = num2cell(localEPamp);      % 5: Peak response
+                    app.respSummary(app.EPcnt,6) = num2cell(0);               % 6: time to peak
+                    app.respSummary(app.EPcnt,7) = num2cell(bl);              % 7: Baseline mean BP1 power
+                    app.respSummary(app.EPcnt,8) = num2cell(rs);              % 8: Response mean BP1 power
+                    app.respSummary(app.EPcnt,9) = num2cell(FDresp);          % 9: Delta BP1 power
+                    app.respSummary(app.EPcnt,10) = num2cell(templateSPW);    % 10: Delta BP1 power                
+                    app.respSummary(app.EPcnt,11) = cellstr(app.expID);   % 11: Patient name-notes        
+                end
+            end    
+            % compute metrics of the mean responses for each stim repeat
+            for ii=1:app.ntemp
+                bltemp = [];
+                rstemp = [];
+                bltemp = app.meanLFP (i,blFrom + halfSweep * (ii-1):blTo + halfSweep * (ii-1));
+                bl = mean (bltemp,2);
+                % the baseline must be subtracted before computing the
+                % absolute value
+                rstemp = abs(app.meanLFP (i,respFrom + halfSweep * (ii-1):respTo + halfSweep * (ii-1)) - bl);
+                % a bit of filtering before computation of the max
+                rstemp = sgolayfilt(rstemp,3,floor(wSgolay/2)+1); %previously framelength
+                [resp, imax] = max (rstemp,[],2);
+                tpeak = app.sp*double(respFrom+imax) + app.timeOffset;
+                resp = resp * sign(app.meanLFP (i,respFrom+imax) - bl);
+                %resp = handles.meanLFP (i,respFrom+imax) - bl;
+
+                % compute the response in the frequency domain    
+                bltemp = [];
+                rstemp = [];
+                bltemp = app.meanEIrat (i,blFromSPG + halfSweepSPG*(ii-1):blToSPG + halfSweepSPG*(ii-1));
+                bl = mean (bltemp,2);
+
+                rstemp = app.meanEIrat (i,rsFromSPG + halfSweepSPG*(ii-1):rsToSPG + halfSweepSPG*(ii-1));
+                rs = mean (rstemp,2);
+                FDresp = rs-bl;
+
+                app.EPcnt = app.EPcnt + 1;
+                app.respSummary(app.EPcnt,1) = cellstr(app.file_in);  % 1: file name
+                app.respSummary(app.EPcnt,2) = num2cell(i);               % 2: channel number
+                app.respSummary(app.EPcnt,3) = num2cell(0);               % 3: trial
+                app.respSummary(app.EPcnt,4) = num2cell(ii);              % 4: repeat
+
+                app.respSummary(app.EPcnt,5) = num2cell(resp);            % 5: Peak response
+                app.respSummary(app.EPcnt,6) = num2cell(tpeak);           % 6: time to peak
+                app.respSummary(app.EPcnt,7) = num2cell(bl);              % 7: Baseline mean BP1 power
+                app.respSummary(app.EPcnt,8) = num2cell(rs);              % 8: Response mean BP1 power
+                app.respSummary(app.EPcnt,9) = num2cell(FDresp);          % 9: Delta BP1 power
+%                handles.respSummary(handles.EPcnt,10) = num2cell(TemplateSPW);    % 10: Delta BP1 power                
+                app.respSummary(app.EPcnt,11) = cellstr(app.expID);   % 11: Patient name-notes        
+            end
+        end        
+    end
+end
+
+app.outEvochedResponses.Data = app.respSummary;
+
+    function gixout = gixres(b)
+        % this nested function computes the residue of the difference
+        % between the template and the given sweep.
+        gixout = sum((segment(blDelta:end)-b*template(blDelta:end)).^2);
+    end
+end
+
+
+function computeMeanData (app)
+    % Compute means of all the data representations on all trials
+    % April 25, 2017. A checkbox is added to skip the first trial
+    % March 2018. Modified to allow conditional computation of BPed data
+
+    leakageFlag = app.displayLeakCk.Value;
+    skipFlag = app.skip1trialCk.Value;
+    if skipFlag, trialBegins = 2;
+    else
+        trialBegins = 1;
+    end
+
+    ntemp = 1; 
+    app.meanSpgDeleaked = [];
+    app.meanSpg = [];
+    for i=1:app.nCh
+        tmp = [];        % initialize tmp
+        % create temporary matrix for average computation
+        tmp (1:app.nTrials-trialBegins+1,1:app.dtaLen) = app.workLFP(i,trialBegins:app.nTrials,1:app.dtaLen);
+        app.meanLFP (i,1:app.dtaLen) = mean (tmp,1);
+        % compute mean of band-passed data
+        for k=1:5
+            if app.BPcomputed(k)
+                tmp (1:app.nTrials-trialBegins+1,1:app.dtaLen) = app.bandPassed_LFP(k,i,trialBegins:app.nTrials,1:app.dtaLen);
+                app.meanBP (k,i,1:app.dtaLen) = mean (tmp,1);
+            end
+        end
+        % compute mean spectrogram
+        tmp = [];
+        if leakageFlag      %GAB: add plotting of deleaked mean spg.
+            tmp (1:app.nTrials-trialBegins+1,1:app.freqN,1:app.spgl) = app.spgDeleaked(i,trialBegins:app.nTrials,1:app.freqN,1:app.spgl);
+            app.meanSpgDeleaked (i,1:app.freqN,1:app.spgl) = mean (tmp,1);
+        end
+        tmp (1:app.nTrials-trialBegins+1,1:app.freqN,1:app.spgl) = app.spg(i,trialBegins:app.nTrials,1:app.freqN,1:app.spgl);
+        app.meanSpg (i,1:app.freqN,1:app.spgl) = mean (tmp,1);
+
+        % compute mean E/I index
+        tmp = [];
+        tmp(1:app.nTrials-trialBegins+1,1:app.spgl) = app.EIrat(i,trialBegins:app.nTrials,1:app.spgl);
+        app.meanEIrat (i,1:app.spgl) = mean (tmp,1);    
+        % compute mean 'gamma' power
+        tmp = [];
+        tmp(1:app.nTrials-trialBegins+1,1:app.spgl) = app.spgPlot(i,trialBegins:app.nTrials,1:app.spgl);
+        app.meanSpgPlot (i,1:app.spgl) = mean (tmp,1);
+
+        % the computation of the mean power spectra has been moved to the
+        % ZebraSpectre.m file    
+    end
+end
---
+++ computeSpectrogram.m
@@ -0,0 +1,140 @@
+function computeSpectrogram(app)
+
+% Hello, my name is me and this is March 3, 2017
+% This code computes the spectograms of all data.
+% If so desired, the SPG is computed after removal of the (large) low
+% frequency transients in order to attenuate the associated spectral
+% leakage.
+
+SPGfrom = app.SPGfromFreq.Value;
+SPGto = app.SPGtoFreq.Value;
+freqStep = app.SPGresolution.Value;
+freq = SPGfrom:freqStep:SPGto;
+app.freqN = (SPGto-SPGfrom)/freqStep + 1;
+window = app.SPGwindow.Value;
+noverlap = app.SPGoverlap.Value;
+leakageFlag = app.leakageCk.Value;
+app.spg=[];
+for i = 1:app.nCh
+    for j= 1:app.nTrials
+        flag = app.decimateFlag.Value;
+        if flag
+            % decimate the input data in case of really long data set or
+            % when using a slow pc...
+            LFPsmall = decimate(app.workLFP(i,j,1:app.dtaLen),10);
+            LFPsmall(i,j,1:app.dtaLen) = decimate(app.workLFP(i,j,1:app.dtaLen),10);
+            [s, w, t, ps] = spectrogram(LFPsmall,hamming(window),noverlap, freq, app.acqF/10, 'yaxis');
+            %t = t + app.timeOffset; %GAB 2019/02/24: offset added
+        else
+            tmp = app.workLFP(i,j,1:app.dtaLen);
+            flagChronux = get(app.ChronuxCk,'Value');
+            if flagChronux
+                params.tapers = [3 5] ;%[10 0.5 1];
+                params.Fs = app.acqF;
+                params.fpass = [SPGfrom SPGto];
+                [rollingWin, overlap] = setSPGwindow (app, app.dtaLen*app.sp);
+                [ps, t, w] = mtspecgramc(tmp,[rollingWin overlap],params);
+                l = size (ps);
+                app.spgl = l(1);
+                app.freqN = l(2);
+                ps = ps';
+                app.spg(i,j,1:app.freqN,1:app.spgl) = ps (1:app.freqN,1:app.spgl);
+                if leakageFlag
+                    % first: subtract the LP filtered data from the workLFP
+                    % data. Second: compute its SPG
+                    tmp = squeeze(app.workLFP(i,j,:))-squeeze(app.bandPassed_LFP(6,i,j,:));
+                    [ps, t, w] = mtspecgramc(tmp,[rollingWin overlap],params);
+                    ps = ps';
+                    app.spgDeleaked(i,j,1:app.freqN,1:app.spgl) = ps (1:app.freqN,1:app.spgl);
+                    app.deleakedFlag = 1;
+                end
+            else
+                [s, w, t, ps] = spectrogram(tmp,hamming(window),noverlap, freq, app.acqF, 'yaxis');
+                l = size (ps);
+                app.spgl = l(2);
+                app.spg(i,j,1:app.freqN,1:app.spgl) = ps (1:app.freqN,1:app.spgl);
+                if leakageFlag
+                    tmp = squeeze(app.workLFP(i,j,:))-squeeze(app.bandPassed_LFP(6,i,j,:));
+                    [s, w, t, ps] = spectrogram(tmp,hamming(window),noverlap, freq, app.acqF, 'yaxis');
+                    app.spgDeleaked(i,j,1:app.freqN,1:app.spgl) = ps (1:app.freqN,1:app.spgl);
+                    app.deleakedFlag = 1;
+                end
+            end
+        end
+        % compute the short time E/I index
+        % First: create the array containing the indexes of the spectral density in the interval 20-50 Hz
+        [segmentI, segmentF, freqCnt] = findSPGelements (w, app.freqN, 20, 50);
+        % Second: loop on all time point of the spg to compute the linear
+        % fit to the spectra segment
+        for ti=1:app.spgl
+            spectraSegment = app.spg(i,j,segmentI(1):segmentI(freqCnt),ti);
+            % compute now the linear fit to the log log segment
+            segmentF = squeeze(segmentF);   % remove the singleton dimensions
+            spectraSegment = squeeze(spectraSegment);
+            % compute slope and offset of the fit
+            %X = [ones(length(segmentF),1) segmentF'];
+            X = [ones(length(segmentF),1) log(segmentF)'];
+            fit = X\log(spectraSegment);
+            app.EIrat (i,j,ti) = fit(2);
+            % the linear regression operators '\' needs row vectors.
+        end    
+        % compute the integral of the SPG in 'gamma' band
+        PWfrom = app.envSPGfrom.Value;
+        PWto = app.envSPGto.Value;        
+        [segmentI, segmentF, freqCnt] = findSPGelements (w, app.freqN, PWfrom, PWto);
+        app.spgPlot(i,j,1:app.spgl) = sum(app.spg(i,j,segmentI(1):segmentI(freqCnt),1:app.spgl));
+        if leakageFlag
+            app.spgPlotDeleaked(i,j,1:app.spgl) = sum(app.spgDeleaked(i,j,segmentI(1):segmentI(freqCnt),1:app.spgl));            
+        end    
+    end    
+end
+app.spgt = t + app.timeOffset; %GAB 2019/02/24: offset added
+app.spgw = w;
+
+function [segmentI, segmentF, freqCnt] = findSPGelements (w, freqNum, lowFreq, highFreq)
+% Given a frequency range (lowFreq and highFreq) identify the elements of
+% the spectrogram that fall within this range. The function returns an array
+% (freqIndex) containing the indexes of the elements, and an array
+% containing the relative frequencies.
+
+segmentI = [];
+segmentF = [];
+
+freqCnt = 0;
+for fj=1:freqNum
+    if w(fj)>highFreq      % interupt the iterations if we pass the frequency limit
+        break
+    end
+    if w(fj)>=lowFreq
+        freqCnt = freqCnt + 1;
+        segmentI(freqCnt) = fj;
+        segmentF(freqCnt) = w(fj);
+    end
+end   
+
+function [wnd ovl] = setSPGwindow (app, deltaT)
+% This function reads (from GUI) or generates the values of window size and
+% overlap to be used for the spectrogram computation.
+% The automatic computation uses the width of the data to adapt the width
+% of the rolling window.
+
+flagAuto = app.ChronuxAutoConfigCk.Value;
+if flagAuto
+    if deltaT<5, wnd = 0.1;
+    else
+        if deltaT<10, wnd = 0.2;
+        else
+            if deltaT<30, wnd = 0.2;
+            else
+                if deltaT<100, wnd = 0.5;
+                else
+                    wnd = 0.5;
+                end
+            end
+        end
+    end
+    ovl = wnd/5;
+else
+    wnd = app.ChronuxRW.Value;
+    ovl = app.ChronuxOvl.Value;
+end    
